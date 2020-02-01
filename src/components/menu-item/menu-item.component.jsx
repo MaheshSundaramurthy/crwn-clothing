1c307ec2cb503e7bfc70c2747de6138e5e4488dc
MaheshSundaramurthy <mahesh.sundaramurthy@touchqlik.com>
2020-02-01 18:44:15 -0500

    Added Routes

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div
     style={{ backgroundImage: `url(${imageUrl})` }}
     className={`menu-item ${size}`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
     <div className="content">
       <h1 className="title">{title}</h1>
@@ -13,4 +15,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
